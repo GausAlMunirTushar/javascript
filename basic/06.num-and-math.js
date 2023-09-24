7898ad1cdcad8c1f0c30ab097a8f82173c68cae6
Gaus Al Munir Tushar <gauslamunirtushar@gmail.com>
2023-09-25 00:30:38 +0600

    number and math

---
+++ basic/06.num-and-math.js
@@ -0,0 +1,31 @@
+// ====================== Numbers =======================
+const score = 400
+// console.log(score)
+
+const balance = new Number(400)
+// console.log(balance.toString());
+// console.log(balance.toFixed(1))
+
+const otherNumber = 23.54555
+// console.log(otherNumber.toPrecision(3))
+
+const thousand = 100000
+// console.log(thousand.toString())
+// console.log(thousand.toLocaleString())
+// console.log(thousand.toLocaleString('bn-BD'))
+
+
+// ==================== Maths ====================
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(2.3))
+// console.log(Math.ceil(2.3))
+// console.log(Math.floor(2.3))
+// console.log(Math.min(4, 5, 6, 2))
+console.log(Math.random())
+console.log(Math.round(Math.random()*10))
+
+const min = 10
+const max = 20
+
+Math.random()
