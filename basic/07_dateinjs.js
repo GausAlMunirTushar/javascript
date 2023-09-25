99bda3f712ffd97cca788fb1eba63f08c431a973
Gaus Al Munir Tushar <gauslamunirtushar@gmail.com>
2023-09-25 22:15:33 +0600

    date and time in js

---
+++ basic/07_dateinjs.js
@@ -0,0 +1,18 @@
+// Dates and Times in depth
+
+let myDate = new Date();
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+
+let newDate = new Date()
+console.log(newDate.getFullYear())
