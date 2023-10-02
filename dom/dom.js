df4dcd5bf54c4d3addd547086679ef912e5553bd
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2023-10-02 06:46:10 +0600

    dom practice

---
+++ dom/dom.js
@@ -0,0 +1,24 @@
+// DOM introduction in javascript
+// DOM - Events
+// All DOM selectors NodeList and HTMLCollection
+// Create Elements
+// Edit and remove elements in DOM
+// Lets build 4 javascript projects for beginners
+
+/* 
+******* Selectors ********
+* getElementById 
+* querySelector
+* querySelectorAll
+* getElementsByClassName
+*/
+
+// getAttribute
+// setAttribute
+
+
+// textContent vs innerHTML vs innerText
+
+// NodeList vs HTML Collection
+
+// How to create a new element in DOM 
